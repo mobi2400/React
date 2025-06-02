db4ca93871b0d21169ab8b1bb5e768cee3f7f78d
mobasshir <mobasshirkhan9931@gmail.com>
2025-06-03 00:31:56 +0530

    started with virtual dom

--- 279-intro-to-jsx/src/index.jsx
+++ 279-intro-to-jsx/src/index.jsx
@@ -3,3 +3,19 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(<div>
+    <h1>Hello world</h1>
+    <p>this is a paragraph</p>
+    </div>,
+    document.getElementById("root"),
+);
+
+/* instead of this one in vanila JS it will be written as
+var h1 = document.createElement("h1");
+h1.innerHTML = "Hello World";
+document.getElementById("root").appendChild(h1);
+*/
