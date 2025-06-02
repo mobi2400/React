2afb2dbda4a5e9f80305ffdf0202bc5530dccab9
mobasshir <mobasshirkhan9931@gmail.com>
2025-06-03 00:39:43 +0530

    completed exercise

--- 280-jsx-code-challenge/src/index.jsx
+++ 280-jsx-code-challenge/src/index.jsx
@@ -8,3 +8,18 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+    <div>
+        <h1>list item</h1>
+        <ul>
+            <li>cake</li>
+            <li>patties</li>
+            <li>burger</li>
+        </ul>
+    </div>,
+    document.getElementById("root"),
+)
