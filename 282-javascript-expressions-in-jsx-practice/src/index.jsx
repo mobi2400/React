e657efe6951c4a41f84938028b171f7cb831550c
mobasshir <mobasshirkhan9931@gmail.com>
2025-06-03 23:47:06 +0530

    completed exercise

--- 282-javascript-expressions-in-jsx-practice/src/index.jsx
+++ 282-javascript-expressions-in-jsx-practice/src/index.jsx
@@ -12,3 +12,14 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+
+import React from "react";
+import reactDom from "react-dom";
+let date = new Date();
+let year = date.getFullYear();
+const name = "Md Mobasshir Shakil Khan"
+reactDom.render(<div>
+    <h1> hello {name}</h1>
+    <p>Copyright {year}</p>
+</div>
+    ,document.getElementById("root"))
