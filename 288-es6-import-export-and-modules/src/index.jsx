e31b9af786f82ecdb99306364ea60998d88ef37a
mobasshir <mobasshirkhan9931@gmail.com>
2025-06-05 15:53:19 +0530

    import all functionn from math.js ad whole and named it as pi

--- 288-es6-import-export-and-modules/src/index.jsx
+++ 288-es6-import-export-and-modules/src/index.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PI,{doublePi,triplePi} from "./math.js"
+import * as pi from "./math.js"
 
 ReactDOM.render(
   <ul>
-    <li>{PI}</li>
-    <li>{doublePi()}</li>
-    <li>{triplePi()}</li>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
