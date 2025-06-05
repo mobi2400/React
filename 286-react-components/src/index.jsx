a5fa489b891b243a0992bbfd3ef6c7042d996f98
mobasshir <mobasshirkhan9931@gmail.com>
2025-06-05 15:02:20 +0530

    used seperate jsx file to make elements

--- 286-react-components/src/index.jsx
+++ 286-react-components/src/index.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-// function name shiuld start with capital letter
-
-function Heading(){
-  return <h1> My Favourite food</h1>
-}
+import Heading from "./Heading";
+import List from "./List";
+// function name should start with capital letter
+// same as<Heading></Heading>
 ReactDOM.render(
   <div>
-   <Heading/> // same as <Heading></Heading>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+   <Heading/> 
+   <List/>
   </div>,
   document.getElementById("root")
 );
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
