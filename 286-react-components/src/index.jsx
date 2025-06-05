9c5bb7d8527b8828629737e073f482cdae1cd9ed
mobasshir <mobasshirkhan9931@gmail.com>
2025-06-05 15:06:54 +0530

    made code more readable and created App file to store ue function

--- 286-react-components/src/index.jsx
+++ 286-react-components/src/index.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./Heading";
-import List from "./List";
+import App from "./App";
+
 // function name should start with capital letter
 // same as<Heading></Heading>
-ReactDOM.render(
-  <div>
-   <Heading/> 
-   <List/>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App/>,document.getElementById("root"));
 
 
