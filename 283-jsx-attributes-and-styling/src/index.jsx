8cd1df216c624ee4b71374ff0a91976cebdcd322
mobasshir <mobasshirkhan9931@gmail.com>
2025-06-05 13:53:11 +0530

    used css in jsx by adding className

--- 283-jsx-attributes-and-styling/src/index.jsx
+++ 283-jsx-attributes-and-styling/src/index.jsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading" contentEditable="false">My Favourite Foods</h1>
+    <div>
+      <img  className="food-img" src="https://madscookhouse.com/wp-content/uploads/2021/01/Subz-Vegetable-Biryani.jpg" alt="biryani" />
+      <img  className="food-img" src="https://images.themodernproper.com/production/posts/2022/Homemade-French-Fries_8.jpg?w=800&q=82&auto=format&fit=crop&dm=1662474181&s=70c29a2dbd0cfbac22bb3fdedf6fbd29" alt="" />
+      <img  className="food-img" src="https://www.foodandwine.com/thmb/DI29Houjc_ccAtFKly0BbVsusHc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/crispy-comte-cheesburgers-FT-RECIPE0921-6166c6552b7148e8a8561f7765ddf20b.jpg" alt="" />
+    </div>
   </div>,
   document.getElementById("root")
 );
