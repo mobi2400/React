88824ff56f58bf36e93bed084408056017ba295a
mobasshir <mobasshirkhan9931@gmail.com>
2025-06-11 10:29:48 +0530

    revised basic iteration function

--- 300-map-filter-reduce/src/index.jsx
+++ 300-map-filter-reduce/src/index.jsx
@@ -1,17 +1,23 @@
-var numbers = [3, 56, 2, 48, 5];
+// var numbers = [3, 56, 2, 48, 5];
 
-//Map -Create a new array by doing something with each item in an array.
+// //map -- iterate whole array and push tahe value into x
+// const NewNumber = numbers.map((x)=>{return x*2})
 
-//Filter - Create a new array by keeping the items that return true.
 
-//Reduce - Accumulate a value by doing something to each item in an array.
+// //filters-- return the value which which overpass certain criteria
+// const greaterNumber=numbers.filter((num)=>{return num >10})
 
-//Find - find the first item that matches from an array.
 
-//FindIndex - find the index of the first item that matches.
+// //reduce--- accumate a value by doing something to each of items in the array
+// const ReduceNUmber=numbers.reduce((accumalator,currentNum)=>{
+//     return accumalator+currentNum
+// })
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
+// //Find -- find the first item that matches from an array
+// const FindNumber=numbers.find((num)=>{return num > 10})
+
+
+// //FindIndex-- return the index of the given num in the array
+// const FindIndexNum = numbers.FindIndexNum((num)=>{return num >10})
+
