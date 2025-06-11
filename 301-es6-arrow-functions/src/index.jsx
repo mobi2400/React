f45a65040c9b0b939a576358e8b8739efbf8bff2
mobasshir <mobasshirkhan9931@gmail.com>
2025-06-12 01:16:26 +0530

    short down arrow function

--- 301-es6-arrow-functions/src/index.jsx
+++ 301-es6-arrow-functions/src/index.jsx
@@ -7,9 +7,7 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // var numbers = [3, 56, 2, 48, 5];
 
 ////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+// newNumbers = numbers.map(x => x*2);
 
 //////Filter - Create a new array by keeping the items that return true.
 // const newNumbers = numbers.filter(function(num) {
@@ -31,8 +29,4 @@ ReactDOM.render(<App />, document.getElementById("root"));
 //   return num > 10;
 // })
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
